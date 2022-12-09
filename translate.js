aa966edb0c9831acb6a366cf470eb62d34158b76
MRIDUL ARORA <aroramridul18@gmail.com>
2022-12-09 11:46:01 +0530

    style: modified to match the parent portfolio site

--- translate.js
+++ translate.js
@@ -2,13 +2,19 @@ var btnTrans = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#txt-output");
 var txtErrorMsg = document.querySelector("#txt-errormsg");
+var outputBox = document.querySelector("#outputbox");
 
  var url = "https://api.funtranslations.com/translate/klingon.json"
 //var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
+txtErrorMsg.style.display ="none";
+outputBox.style.display = "none";
+
 function errorHandler(error){
     console.log("Error: " + error);
     txtErrorMsg.innerHTML = "Error: " + error;
+    txtErrorMsg.style.display = "block";
+    outputBox.style.display = "none";
 }
 
 function clickHandler() {
@@ -17,10 +23,11 @@ function clickHandler() {
 
     function divOutput(out){        
         if(out.error) {
-                        
+            errorHandler(out.error.message);     
         }else if(out.success){
-            var displayOutput = "Translated text to klingon is as... ";
-            txtOutput.innerHTML = displayOutput + out.contents.translated;
+            txtOutput.innerHTML = out.contents.translated;
+            outputBox.style.display = "block";
+            txtErrorMsg.style.display ="none";
         }        
     }
 
